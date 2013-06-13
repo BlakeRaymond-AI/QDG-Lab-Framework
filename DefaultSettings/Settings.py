69207be459dafbfe538e293dca6e80e6a26998cb
Victor I. Barua <victor.barua@gmail.com>
2013-06-12 17:12:08 -0700

    Settings dicts can now be saved and output nicely.

--- DefaultSettings/Settings.py
+++ DefaultSettings/Settings.py
@@ -1,3 +1,7 @@
+from pprint import PrettyPrinter
+from os import path
+from pickle import dump
+
 class NonExistentValueError(Exception):
 	'''
 	Raised by Settings object when modification of an attribute that is not
@@ -25,4 +29,23 @@ class Settings(dict):
 			dict.__setitem__(self, key, val)
 		else:
 			raise NonExistentValueError(key)
+			
+	def __str__(self):
+		pp = PrettyPrinter(indent=0)
+		return pp.pformat(self)
+		
+	def save(self, filePath, fname = 'Settings'):
+		txtPath = path.join(filePath, ''.join([fname, '.txt']))
+		f = open(txtPath, 'wb')
+		f.write(str(self))
+		f.close()
+		picklePath = path.join(filePath, ''.join([fname, '.pkl']))
+		f = open(picklePath, 'wb')
+		dump(self, f)
+		f.close()
+
+	
+			
+				
+		
 			
