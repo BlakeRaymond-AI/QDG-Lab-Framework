c1e2b8adc1a1859be32590c7abc0add015dd85a0
Victor I. Barua <victor.barua@gmail.com>
2013-07-11 14:13:28 -0700

    Testing continuous data collection.

--- DeviceMediators/DeviceControllers/Stabil_Ion_Controller.py
+++ DeviceMediators/DeviceControllers/Stabil_Ion_Controller.py
@@ -1,48 +1,63 @@
 from serial import Serial
-from time import sleep
+from time import sleep, time
 
 class Stabil_Ion_Controller(Serial):
+	
 	def __init__(self, port = 0):
 		super(Stabil_Ion_Controller, self).__init__(port = port, timeout = 0)
-
-	def read(self, size = 1):
+		self.flushInput()
+		
+	def read(self, size = 32):
 		data = super(Stabil_Ion_Controller, self).read(size)
 		data = data[:-2]
 		return data
 		
 	def IG1On(self):
 		self.write("IG1 ON \r\n")
-		self.read(32)	
+		sleep(0.5)
+		self.flushInput()
 	
 	def IG1Off(self):
 		self.write("IG1 OFF \r\n")
-		self.read(32)	
-	
+		sleep(0.5)
+		self.flushInput()
+		
 	def IG2On(self):
 		self.write("IG2 ON \r\n")
-		self.read(32)
+		sleep(0.5)
+		self.flushInput()
 		
 	def IG2ff(self):
 		self.write("IG2 OFF \r\n")
-		self.read(32)			
-		
+		sleep(0.5)
+		self.flushInput()
+	
 	def degasOn(self):
 		self.write("DG ON \r\n")
-		self.read(32)
-		
+		sleep(0.5)
+		self.flushInput()
+				
 	def getIG1Pressure(self):
 		self.write("DS IG1 \r\n")
 		sleep(0.1)
-		data = self.read(32)
+		data = self.read()
 		value = float(data)
 		return value
 	
 	def getIG2Pressure(self):
 		self.write("DS IG2 \r\n")
-		data = self.read(32)
+		data = self.read()
 		sleep(0.1)
 		value = float(data)
-		return value	
+		return value
 
-SIC = Stabil_Ion_Controller(7)
-print SIC.getIG1Pressure()
+
+SIC = Stable_Ion_Controller(7)
+tStart = time()
+collectionDuration_s = 60
+while (time() - collectionDuration > tStart):
+	print SIC.getIG1Pressure()
+	sleep = 4
+
+		
+			
