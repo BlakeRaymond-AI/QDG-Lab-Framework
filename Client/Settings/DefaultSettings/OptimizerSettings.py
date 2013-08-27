31e3a9cad6d8ac963ccd60072c7dc5eaed4d500a
Victor I. Barua <victor.barua@gmail.com>
2013-08-26 17:25:30 -0700

    Updated optimiser settings.

--- Client/Settings/DefaultSettings/OptimizerSettings.py
+++ Client/Settings/DefaultSettings/OptimizerSettings.py
@@ -6,21 +6,18 @@ OptimizerSettings['numOfParticles'] = 0		# Number of particles in a generation.
 OptimizerSettings['numOfGenerations'] = 0	# Number of generations.
 OptimizerSettings['phiG'] = 1	# Velocity weight towards best global particle.
 OptimizerSettings['phiP'] = 1	# Velocity weight towards best local particle.
-OptimizerSettings['w'] = 1	#
-OptimizerSettings['speedLimiter'] = 1 # Limits the max speed of a particle in 
-			# any of it's dimensions to speedLimiter * (upperBound - lowerBound)
+OptimizerSettings['w'] = 1		# Velocity damping factor.
+OptimizerSettings['alpha'] = 1 # Limits the max speed of a particle in any of it's dimensions to alpha * (upperBound - lowerBound)
 OptimizerSettings['minimization'] = False	# Toggles minimization or maximization. 
 
-# paramBounds is an n-tuple of 2-tuples representing the lower and upper bounds  
-# of paramaters
+# paramBounds is an n-tuple of 2-tuples representing the lower and upper bounds of paramaters
 paramBounds = (
 	# (lowerBound1, upperBound1),
 	# (lowerBound2, upperBound2),
 	# ...
 )
+OptimizerSettings['paramBounds'] = paramBounds
 
 OptimizerSettings['takeData'] = False	# Won't actually take data, but will enable optimizer.
+OptimizerSettings['dataFolderName'] = "OptimizerData"
 OptimizerSettings['persistent'] = True
-
-
-OptimizerSettings['paramBounds'] = paramBounds
